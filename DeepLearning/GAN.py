a0fbf7f3967879d0160020681d5b2ebd7aeb3afe
Hod <hod.rivenzon@gmail.com>
2021-02-25 13:38:09 +0200

    Done

--- DeepLearning/GAN.py
+++ DeepLearning/GAN.py
@@ -139,48 +139,6 @@ class GAN:
         plt.show()
 
 
-
-    # def train(self, epochs=1, batchSize=128):
-    #     batchCount = int(X_train.shape[0] / batchSize)
-    #     print('Epochs:', epochs)
-    #     print('Batch size:', batchSize)
-    #     print('Batches per epoch:', batchCount)
-    #     # 4. Build a training loop in which you iterate between a training step on the Generator
-    #     # network and a training step on the discriminator network
-    #     for e in range(1, epochs + 1):
-    #         print('-' * 15, 'Epoch %d' % e, '-' * 15)
-    #         for _ in tqdm(range(batchCount)):
-    #             # Get a random set of input noise and images
-    #             noise = np.random.normal(0, 1, size=[batchSize, randomDim])
-    #             imageBatch = X_train[np.random.randint(0, X_train.shape[0], size=batchSize)]
-    #
-    #             # Generate fake MNIST images
-    #             generatedImages = generator.predict(noise)
-    #             # print np.shape(imageBatch), np.shape(generatedImages)
-    #             X = np.concatenate([imageBatch, generatedImages])
-    #
-    #             # Labels for generated and real data
-    #             yDis = np.zeros(2 * batchSize)
-    #             # One-sided label smoothing
-    #             yDis[:batchSize] = 0.9
-    #
-    #             # Train discriminator
-    #             discriminator.trainable = True
-    #             dloss = discriminator.train_on_batch(X, yDis)
-    #
-    #             # Train generator
-    #             noise = np.random.normal(0, 1, size=[batchSize, randomDim])
-    #             yGen = np.ones(batchSize)
-    #             discriminator.trainable = False
-    #             gloss = gan.train_on_batch(noise, yGen)
-    #
-    #         # Store loss of most recent batch from this epoch
-    #         dLosses.append(dloss)
-    #         gLosses.append(gloss)
-
-
-
-
     def CNN(self):
         callbacks = MyCallBack()
 
